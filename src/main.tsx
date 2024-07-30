74f515c26dc8aae02747afcfbb54d12af77d412c
Rafal <rgrajcar@gmail.com>
2024-07-30 17:12:09 +0200

    added ul and li from lesson 19

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,25 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client'
-import './index.css'
+import ReactDOM from 'react-dom/client';
+import './index.css';
+
+const users = [{
+  id: 'a1',
+  name: 'Marcin'
+}, {
+  id: 'a2',
+  name: 'Marian'
+}, {
+  id: 'a3',
+  name: 'Ewa'
+}, {
+  id: 'a4',
+  name: 'Marianna'
+}];
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <div className="container" />
-    <label htmlFor="login">Login</label>
-    <input type="text" id="login"/>
+    <ul>
+      {users.map((user) => (<li>{user.name}</li>))}
+    </ul>
   </React.StrictMode>
 )
