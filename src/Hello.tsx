fdfec6b6bde301c2c3bc61687a00a34697feb64d
Rafal <rgrajcar@gmail.com>
2024-07-31 22:46:24 +0200

    changed to class component

--- src/Hello.tsx
+++ src/Hello.tsx
@@ -1,11 +1,19 @@
+import {Component} from "react";
+
 interface Props {
   name: string;
 }
 
-const Hello = (props: Props) => {
-  return (
-    <h1>Hello {props.name}</h1>
-  )
+// const Hello = (props: Props) => {
+//   return (
+//     <h1>Hello {props.name}</h1>
+//   )
+// }
+
+class Hello extends Component<Props> {
+  render() {
+    return <h1>Hello {this.props.name}</h1>;
+  }
 }
 
 export { Hello };
