5d0cecb3c1ed52bbbdb6e165e483dabf74cd0705
Rafal <rgrajcar@gmail.com>
2024-07-31 22:39:45 +0200

    added new component with prop

--- src/main.tsx
+++ src/main.tsx
@@ -1,30 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-
-const users = [{
-  id: 'a1',
-  name: 'Marcin'
-}, {
-  id: 'a2',
-  name: 'Marian'
-}, {
-  id: 'a3',
-  name: 'Ewa'
-}, {
-  id: 'a4',
-  name: 'Marianna'
-}];
+import {Hello} from "./Hello.tsx";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ul>
-      {users.map((user) => (
-        <li key={user.id}>
-          <strong>{user.name}</strong>
-          <span>{user.id}</span>
-        </li>
-      ))}
-    </ul>
+   <Hello name='Marcin' />
   </React.StrictMode>
 )
