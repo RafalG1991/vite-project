3991373c8fe38e446230c7485f5c04ba8121c70c
Rafal <rgrajcar@gmail.com>
2024-08-01 20:06:45 +0200

    added CartSummary

--- src/Cart.tsx
+++ src/Cart.tsx
@@ -1,10 +1,12 @@
 import { CartHeader } from "./CartHeader";
+import {CartSummary} from "./CartSummary.tsx";
 
 const Cart = () => {
   return (
     <table>
       <CartHeader />
       <tbody></tbody>
+      <CartSummary />
     </table>
   )
 }
---
+++ src/CartSummary.tsx
@@ -0,0 +1,13 @@
+const CartSummary = () => {
+  return (
+    <tfoot>
+      <tr>
+        <td>Łącznie</td>
+        <td></td>
+        <td>90 zł</td>
+      </tr>
+    </tfoot>
+  )
+}
+
+export { CartSummary };
