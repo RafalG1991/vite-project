9853bb9ec699b6f48b630faa886e7027f959cd6b
Rafal <rgrajcar@gmail.com>
2024-08-01 20:02:35 +0200

    added Cart structure

---
+++ src/Cart.tsx
@@ -0,0 +1,16 @@
+const Cart = () => {
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Przedmiot</th>
+          <th>Ilość</th>
+          <th>Cena</th>
+        </tr>
+      </thead>
+      <tbody></tbody>
+    </table>
+  )
+}
+
+export { Cart };
--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import {Hello} from "./Hello.tsx";
-import {ManyValues} from "./ManyValues.tsx";
-import {Foo} from "./Foo.tsx";
+
+import {Cart} from "./Cart.tsx";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Hello name='Marcin' />
-    <ManyValues first={1} second={2} third={3} fourth={4} fifth={5} sixth={6} seventh={8}/>
-    <Foo elements={[{id: 1, value: 10}, {id: 2, value: 5}, {id: 3, value: 11}]} />
+    <Cart />
   </React.StrictMode>
 )
