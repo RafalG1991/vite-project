4492b093ff322b471983cb87adc8d50e5266a7a4
Rafal <rgrajcar@gmail.com>
2024-08-01 20:34:43 +0200

    refactored CartSummary to count total price

--- src/CartSummary.tsx
+++ src/CartSummary.tsx
@@ -1,10 +1,17 @@
-const CartSummary = () => {
+import {CartItem} from "./types.ts";
+
+type Props = {
+  elements: CartItem[];
+}
+
+const CartSummary = ({elements}: Props) => {
+  const total = elements.reduce((acc, curr) => acc + curr.price * curr.quantity, 0);
   return (
     <tfoot>
       <tr>
-        <td>Łącznie</td>
+        <td>Łącznie:</td>
         <td></td>
-        <td>90 zł</td>
+        <td>{total.toFixed(2)} zł</td>
       </tr>
     </tfoot>
   )
