32d69cf7fae89ed013b40c3da7a330789464101d
Rafal <rgrajcar@gmail.com>
2024-08-05 20:07:37 +0200

    added FormData type

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
 import {FormEvent} from "react";
 
+type FormData = {
+  login: {value: string};
+}
+
 export const App = () => {
   const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    console.log("submit!");
+    const { login } = e.target as typeof e.target & FormData;
+    console.log(login.value);
   }
   
   return (
     <form onSubmit={handleSubmit}>
-      <input type="text"/>
+      <input type="text" name="login"/>
+      <button type="submit">Submit!</button>
     </form>
   )
 }
