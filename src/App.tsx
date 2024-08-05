d02ad1e873a3a238b6477c672115b8524427e475
Rafal <rgrajcar@gmail.com>
2024-08-05 20:25:08 +0200

    changed form from uncontrolled to controlled variant

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,27 @@
-import {FormEvent} from "react";
+import {ChangeEvent, FormEvent, useState} from "react";
 
 type FormData = {
   login: {value: string};
 }
 
 export const App = () => {
+  const [login, setLogin] = useState('');
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setLogin(e.target.value);
+  }
+
   const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     const { login } = e.target as typeof e.target & FormData;
     console.log(login.value);
   }
+
+  console.log(login);
   
   return (
     <form onSubmit={handleSubmit}>
-      <input type="text" name="login"/>
+      <input type="text" name="login" value={login} onChange={handleChange} />
       <button type="submit">Submit!</button>
     </form>
   )
