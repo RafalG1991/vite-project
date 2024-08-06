097499b3a465796efa37be51d0a1403cc1bc2e7e
Rafal <rgrajcar@gmail.com>
2024-08-06 19:16:40 +0200

    added form values type and used useForm hook to control the form

--- src/Form.tsx
+++ src/Form.tsx
@@ -1,18 +1,28 @@
-import {useState} from "react";
+import {useForm} from "./hooks/useForm.ts";
+
+type FormValues = {
+  login: string;
+  password: string;
+  acceptRules: boolean;
+}
 
 export const Form = () => {
-  const [login, setLogin] = useState('');
-  const [password, setPassword] = useState('');
-  const [acceptRules, setAcceptRules] = useState('');
+  const [form, handleChange] = useForm<FormValues>({
+    login: '',
+    password: '',
+    acceptRules: false,
+  });
+
+  console.log(form);
 
   return (
     <form>
       <label htmlFor="login">Login: </label>
-      <input type="text" id="login" name="login" value={login}/>
+      <input type="text" id="login" name="login" value={form.login} onChange={handleChange}/>
       <label htmlFor="password">Password: </label>
-      <input type="password" id="password" name="password" value={password}/>
+      <input type="password" id="password" name="password" value={form.password} onChange={handleChange}/>
       <label htmlFor="login">Accept the rules</label>
-      <input type="checkbox" id="acceptRules" name="acceptRules" value={acceptRules}/>
+      <input type="checkbox" id="acceptRules" name="acceptRules" checked={form.acceptRules} onChange={handleChange}/>
     </form>
   )
 }
