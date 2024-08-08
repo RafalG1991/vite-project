8b9e67d4ba4cacfdf0c5b0c47b89a7a1176adf76
Rafal <rgrajcar@gmail.com>
2024-08-08 20:03:09 +0200

    added counter in useEffect to force rerendering child component

--- src/ParentForCallback.tsx
+++ src/ParentForCallback.tsx
@@ -1,9 +1,25 @@
 import {ChildWithFunction} from "./ChildWithFunction.tsx";
+import {useEffect, useState} from "react";
 
 export const ParentForCallback = () => {
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCounter(prevState => prevState + 1);
+    }, 1000);
+
+    return () => {
+      clearInterval(interval);
+    }
+  }, []);
+
   const foo = () => {
     console.log("Hello World!");
   }
 
-  return <ChildWithFunction callback={foo} />
+  return <div>
+    <ChildWithFunction callback={foo} />
+    <p>Counter: {counter}</p>
+  </div>
 }
