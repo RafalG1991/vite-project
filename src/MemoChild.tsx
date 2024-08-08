221a2783b88947863f5110c4725d6d1631f5dcff
Rafal <rgrajcar@gmail.com>
2024-08-08 19:32:12 +0200

    added heavyCalculations function to test the performance without memo

--- src/MemoChild.tsx
+++ src/MemoChild.tsx
@@ -2,6 +2,18 @@ type PropsType = {
   value: number[];
 }
 
+const heavyCalculations = () => {
+  console.log("HEAVY CALC STARTED");
+  for(let i=0; i<1000000000; i++) {}
+  console.log("HEAVY CALC ENDED");
+  return 1;
+}
+
 export const MemoChild = ({value}: PropsType) => {
-  return <p>{value.join()}</p>
+  const calcValue = heavyCalculations();
+
+  return <div>
+    <p>{value.join()}</p>
+    <p>Calc value: {calcValue}</p>
+  </div>
 }
