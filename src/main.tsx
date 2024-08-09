c4c81720812ae795d5de49b0f91d68d8d1711048
Rafal <rgrajcar@gmail.com>
2024-08-09 18:16:59 +0200

    added FetchComponent to main

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
-import {ParentForCallback} from "./ParentForCallback.tsx";
+import {FetchComponent} from "./FetchComponent.tsx";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ParentForCallback />
+    <FetchComponent />
   </React.StrictMode>
 )
