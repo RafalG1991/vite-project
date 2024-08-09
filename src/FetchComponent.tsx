ae7082694aef8721ae7f6fea02ee030d01aa9849
Rafal <rgrajcar@gmail.com>
2024-08-09 18:27:40 +0200

    added getData function for fetching and useEffect

--- src/FetchComponent.tsx
+++ src/FetchComponent.tsx
@@ -8,7 +8,18 @@ type Entity = {
 
 export const FetchComponent = () => {
   const [entities, setEntities] = useState<Entity[] | null>([]);
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(true);
+
+  const getData = async () => {
+    const response = await fetch('http://localhost:3000/posts');
+    const data: Entity[] = await response.json();
+    setEntities(data);
+    setLoading(false);
+  }
+
+  useEffect(() => {
+    getData();
+  }, []);
 
   if(loading) return <p>Loading...</p>
 
