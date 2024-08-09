5ca956d1881477598faa7dfc40d27e9b515f56ba
Rafal <rgrajcar@gmail.com>
2024-08-09 18:16:02 +0200

    added loading state to display loader when data is not ready

--- src/FetchComponent.tsx
+++ src/FetchComponent.tsx
@@ -8,8 +8,9 @@ type Entity = {
 
 export const FetchComponent = () => {
   const [entities, setEntities] = useState<Entity[] | null>([]);
+  const [loading, setLoading] = useState(false);
 
-
+  if(loading) return <p>Loading...</p>
 
   return <ul>
     {entities?.map((el) => <li key={el.id}>{el.title} views: {el.views}</li>)}
