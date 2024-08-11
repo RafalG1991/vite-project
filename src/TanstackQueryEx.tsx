937cf79605d30446d51480009efbf98c3aab80af
Rafal <rgrajcar@gmail.com>
2024-08-11 22:09:03 +0200

    added TanstackQueryEx component to fetch for data from json server

---
+++ src/TanstackQueryEx.tsx
@@ -0,0 +1,21 @@
+import {useQuery} from "@tanstack/react-query";
+
+type Post = {
+  id: string;
+  title: string;
+  views: number;
+}[]
+
+export const TanstackQueryEx = () => {
+  const {data} = useQuery<Post>({
+    queryKey: ['posts'],
+    queryFn: async () => {
+      const response = await fetch('http://localhost:3000/posts');
+      return response.json();
+    }
+  });
+
+  return <ul>
+    {data?.map(el => <li key={el.id}>{el.title} - {el.views}</li>)}
+  </ul>
+}
--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,14 @@ import ReactDOM from 'react-dom/client';
 import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
 import './index.css';
 
-import {FetchComponent} from "./FetchComponent.tsx";
+import {TanstackQueryEx} from "./TanstackQueryEx.tsx";
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <FetchComponent />
+      <TanstackQueryEx />
     </QueryClientProvider>
   </React.StrictMode>
 )
