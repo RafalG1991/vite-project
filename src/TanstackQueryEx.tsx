a13d1bb6229e14548b3592015df1701b596ce6c8
Rafal <rgrajcar@gmail.com>
2024-08-22 21:46:40 +0200

    changed TanstackQueryEx.tsx component to display the query result from both hooks

--- src/TanstackQueryEx.tsx
+++ src/TanstackQueryEx.tsx
@@ -1,11 +1,26 @@
 import {useGetPostsQuery} from "./hooks/useGetPostsQuery.ts";
+import {useGetPosts} from "./hooks/useGetPosts.ts";
 
 export const TanstackQueryEx = () => {
-  const {data} = useGetPostsQuery('1');
+  const {data} = useGetPosts();
+  const firstPostId = data ? data[0].id : undefined;
 
-  if (!data) return null;
+  const {data: subposts} = useGetPostsQuery(firstPostId);
+  console.log(data, subposts);
 
-  return <ul>
-    <li key={data.id}>{data.title} - {data.views}</li>
-  </ul>
+  if (!data || !subposts) return null;
+
+  return <div>
+    <ul>
+      {data.map((el) => (
+        <li key={el.id}>{el.title} - {el.views}</li>
+      ))}
+    </ul>
+    <hr/>
+    <ul>
+      {subposts.map((el) => (
+        <li key={el.id}>{el.title} - {el.views}</li>
+      ))}
+    </ul>
+  </div>
 }
