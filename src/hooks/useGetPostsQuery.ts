08e0fc31dbe3f2ec8b07d0a1407128d9330b1247
Rafal <rgrajcar@gmail.com>
2024-08-22 21:17:18 +0200

    changed custom hook to use key with subposts

--- src/hooks/useGetPostsQuery.ts
+++ src/hooks/useGetPostsQuery.ts
@@ -1,21 +1,18 @@
 import {useQuery} from "@tanstack/react-query";
 
-type Post = {
+type PostWithSubposts = {
   id: string;
   title: string;
   views: number;
-}[]
+}
 
-export const useGetPostsQuery = () => {
-  const {data} = useQuery<Post>({
-    queryKey: ['posts'],
+export const useGetPostsQuery = (postId: string) => {
+  useQuery({
+    queryKey: ['subposts', postId],
     queryFn: async () => {
-      const response = await fetch('http://localhost:3000/posts');
-      return response.json();
+      const response = await fetch(`http://localhost:3000/posts/${postId}?_embed=subposts`);
+      return response.json() as Promise<PostWithSubposts>;
     }
   });
 
-  return {
-    data,
-  }
 }
