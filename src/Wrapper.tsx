be6849df2820bb5e558c5617bc95789b51d33b98
Rafal <rgrajcar@gmail.com>
2024-08-25 19:43:57 +0200

    added clsx for conditional styling

--- src/Wrapper.tsx
+++ src/Wrapper.tsx
@@ -1,12 +1,13 @@
 import {ReactNode} from "react";
 import styles from './Wrapper.module.scss';
+import clsx from "clsx";
 
 type PropWithChildren = {
   children: ReactNode;
 }
 
 export const Wrapper = ({children}: PropWithChildren) => {
-  return <div className={styles.wrapper}>
+  return <div className={clsx(styles.wrapper)}>
     {children}
   </div>
 }
