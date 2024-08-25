f2299013af5ec6035e8bcc968a3f4eb0fe999870
Rafal <rgrajcar@gmail.com>
2024-08-25 18:58:33 +0200

    added scss styles for Wrapper component and added it to main

--- src/Wrapper.tsx
+++ src/Wrapper.tsx
@@ -1,11 +1,12 @@
 import {ReactNode} from "react";
+import './Wrapper.scss';
 
 type PropWithChildren = {
   children: ReactNode;
 }
 
 export const Wrapper = ({children}: PropWithChildren) => {
-  return <div>
+  return <div className="wrapper">
     {children}
   </div>
 }
--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,16 @@ import ReactDOM from 'react-dom/client';
 import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
 import './index.css';
 
-import {Books} from "./Books.tsx";
+import {Wrapper} from "./Wrapper.tsx";
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <Books />
+      <Wrapper>
+        <h1>Lorem Ipsum</h1>
+      </Wrapper>
     </QueryClientProvider>
   </React.StrictMode>
 )
