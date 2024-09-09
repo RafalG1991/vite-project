5a627112cd08c7b83468160b32c67327d587c52c
Rafal <rgrajcar@gmail.com>
2024-09-09 19:08:30 +0200

    added AboutCompany view

---
+++ src/views/AboutCompany.tsx
@@ -0,0 +1,9 @@
+export const AboutCompany = () => {
+  return (
+    <div>
+      <h1>About our company</h1>
+      <h2>Make life easier is our mission!</h2>
+      <h3>We are looking for new projects to be created for you</h3>
+    </div>
+  )
+}
