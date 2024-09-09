40869278048fe745b76030a087d6b94c8b862d3f
Rafal <rgrajcar@gmail.com>
2024-09-09 19:02:50 +0200

    added About view for subpage

---
+++ src/views/About.tsx
@@ -0,0 +1,9 @@
+export const About = () => {
+  return (
+    <div>
+      <h1>About our mission</h1>
+      <h2>Our mission is to help you discover the beauty of the world</h2>
+      <h3>Join us to learn more about our mission!</h3>
+    </div>
+  )
+}
