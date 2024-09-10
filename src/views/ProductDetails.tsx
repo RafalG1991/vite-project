8c193adb7c1a7c566da2f73f98b548a80a523f36
Rafal <rgrajcar@gmail.com>
2024-09-10 18:37:58 +0200

    added useNavigate hook to navigate instantly to the /product path

--- src/views/ProductDetails.tsx
+++ src/views/ProductDetails.tsx
@@ -1,6 +1,15 @@
-import {Link} from "react-router-dom";
+import {Link, useNavigate} from "react-router-dom";
+import {useEffect} from "react";
 
 export const ProductDetails = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    navigate('..', {
+      relative: 'path',
+    });
+  }, []);
+
   return (
     <div>
       <h5>Product details</h5>
