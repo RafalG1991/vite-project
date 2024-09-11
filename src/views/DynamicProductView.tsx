3c529ff469c03d5b1113104112feb0651550a67c
Rafal <rgrajcar@gmail.com>
2024-09-11 15:56:50 +0200

    added useLocation hook to show pathname

--- src/views/DynamicProductView.tsx
+++ src/views/DynamicProductView.tsx
@@ -1,11 +1,13 @@
-import {useParams} from "react-router-dom";
+import {useLocation, useParams} from "react-router-dom";
 
 export const DynamicProductView = () => {
   const { productId, additionalParam} = useParams<{productId: string, additionalParam: string}>();
+  const { pathname } = useLocation();
   return (
     <div>
       <h1>Product</h1>
       <p>Product - ID: {productId} - {additionalParam}</p>
+      <p>Pathname: {pathname}</p>
     </div>
   )
 }
