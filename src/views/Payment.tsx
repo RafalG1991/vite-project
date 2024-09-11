11ab31007e810c33c57561d3117fe407e4f19424
Rafal <rgrajcar@gmail.com>
2024-09-11 16:10:43 +0200

    added useSearchParams hook to read the params and show it in Payment component

--- src/views/Payment.tsx
+++ src/views/Payment.tsx
@@ -1,8 +1,14 @@
+import {useSearchParams} from "react-router-dom";
+
 export const Payment = () => {
+  const [params] = useSearchParams();
+  const [id, status] = [...params.entries()];
   return (
     <div>
-      <p>Payment ID:</p>
-      <p>Payment status:</p>
+      {/*<p>Payment ID: {params.get('paymentId')}</p>*/}
+      {/*<p>Payment status: {params.get('paymentStatus')}</p>*/}
+      <p>Payment ID: {id[1]}</p>
+      <p>Payment status: {status[1]}</p>
     </div>
   )
 }
