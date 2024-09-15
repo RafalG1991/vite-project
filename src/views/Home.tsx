fe73b1d0d99ed54abd1c4c0e2c240e92dd587ed6
Rafal <rgrajcar@gmail.com>
2024-09-15 20:49:30 +0200

    added Form component to Home view with input and button

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,8 +1,14 @@
+import {Form} from "react-router-dom";
+
 export const Home = () => {
   return (
     <div>
       <h1>Home Page</h1>
       <h2>Welcome on home page!</h2>
+      <Form method="post" action="create">
+        <input type="text" name="title"/>
+        <button type="submit">Submit</button>
+      </Form>
     </div>
   )
 }
