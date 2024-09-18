7ecb24ba1fff6068cbd062a1fd33bb996da3a7f5
Rafal <rgrajcar@gmail.com>
2024-09-18 21:52:59 +0200

    added useLoaderData hook to load the data from loader with SingleTask type

---
+++ src/types/tasks.ts
@@ -0,0 +1,5 @@
+export type SingleTask = {
+  id: number;
+  title: string;
+  description: string;
+}
--- src/views/TasksView.tsx
+++ src/views/TasksView.tsx
@@ -1,7 +1,14 @@
+import {useLoaderData} from "react-router-dom";
+import {SingleTask} from "../types/tasks.ts";
+
 export const TasksView = () => {
+  const data = useLoaderData() as SingleTask[];
   return (
     <div>
       <h1>Tasks</h1>
+      <ul>
+        {data.map(el => <li key={el.id}>{el.title}</li>)}
+      </ul>
     </div>
   )
 }
