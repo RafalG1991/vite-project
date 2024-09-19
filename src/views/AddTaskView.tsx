d98647a682b3a1bc4f0342a5b6dca02ca2a6388d
Rafal <rgrajcar@gmail.com>
2024-09-19 20:06:55 +0200

    added Form with input, textarea and button

--- src/views/AddTaskView.tsx
+++ src/views/AddTaskView.tsx
@@ -1,7 +1,14 @@
+import {Form} from "react-router-dom";
+
 export const AddTaskView = () => {
   return (
     <div>
       <h1>Create new task</h1>
+      <Form method="post">
+        <input type="text" name="title"/>
+        <textarea name="description" />
+        <button type="submit">Add task</button>
+      </Form>
     </div>
   )
 }
