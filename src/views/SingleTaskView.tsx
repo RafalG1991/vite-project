344beca27d98512c585c2d6c299d8fa985a7bafe
Rafal <rgrajcar@gmail.com>
2024-09-22 21:26:39 +0200

    added delete button to SingleTaskView to handle delete

--- src/views/SingleTaskView.tsx
+++ src/views/SingleTaskView.tsx
@@ -1,13 +1,15 @@
-import {useLoaderData} from "react-router-dom";
+import {useLoaderData, useSubmit} from "react-router-dom";
 import {SingleTask} from "../types/tasks.ts";
 
 export const SingleTaskView = () => {
   const {title, description} = useLoaderData() as SingleTask;
+  const submit = useSubmit();
 
   return (
     <div>
       <h1>{title}</h1>
       <p>{description}</p>
+      <button>Delete task</button>
     </div>
   )
 }
