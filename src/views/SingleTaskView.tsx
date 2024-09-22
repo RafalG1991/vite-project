f0433032ec78f978b28ae3eb40848b249bc72e4a
Rafal <rgrajcar@gmail.com>
2024-09-22 21:33:56 +0200

    added handleDelete function to send data by useSubmit hook to remove selected task

--- src/views/SingleTaskView.tsx
+++ src/views/SingleTaskView.tsx
@@ -2,14 +2,23 @@ import {useLoaderData, useSubmit} from "react-router-dom";
 import {SingleTask} from "../types/tasks.ts";
 
 export const SingleTaskView = () => {
-  const {title, description} = useLoaderData() as SingleTask;
+  const {id, title, description} = useLoaderData() as SingleTask;
   const submit = useSubmit();
+
+  const handleDelete = () => {
+    submit({
+      id,
+    }, {
+      method: "DELETE",
+      action: "/",
+    });
+  }
 
   return (
     <div>
       <h1>{title}</h1>
       <p>{description}</p>
-      <button>Delete task</button>
+      <button onClick={handleDelete}>Delete task</button>
     </div>
   )
 }
