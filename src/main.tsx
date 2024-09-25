ed3052d4cca6c76e9b0e4040d8005b5dc91efb12
Rafal <rgrajcar@gmail.com>
2024-09-25 20:22:22 +0200

    added tanstack react-router config in main with RouterProvider

--- src/main.tsx
+++ src/main.tsx
@@ -2,17 +2,20 @@ import React, {lazy, Suspense} from 'react';
 import ReactDOM from 'react-dom/client';
 import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
 
+import { createRouter, RouterProvider} from "@tanstack/react-router";
+import { routeTree } from "./routeTree.gen";
+
 import {StyledWrapper} from "./StyledWrapper.tsx";
 import {ThemeProvider} from "styled-components";
-import {
-  BrowserRouter,
-  createBrowserRouter,
-  createRoutesFromElements,
-  Navigate,
-  Route,
-  RouterProvider,
-  Routes
-} from "react-router-dom";
+// import {
+//   BrowserRouter,
+//   createBrowserRouter,
+//   createRoutesFromElements,
+//   Navigate,
+//   Route,
+//   RouterProvider,
+//   Routes
+// } from "react-router-dom";
 import {Home} from "./views/Home.tsx";
 import {AboutUs} from "./views/AboutUs.tsx";
 // import About from "./views/About.tsx";
@@ -95,63 +98,74 @@ const theme = {
 //   },
 //   ]);
 
-const About = lazy(() => import ("./views/About"));
+// const About = lazy(() => import ("./views/About"));
+//
+// const router2 = createBrowserRouter(
+//   createRoutesFromElements(
+//     <Route element={<MainLayout />}>
+//       <Route path="/" element={<Home />}/>
+//       <Route
+//         path="/create"
+//         element={<Create />}
+//         action={async ({request}) => {
+//           const data = await request.formData();
+//           console.log('Lorem ipsum', data.get('title'));
+//           return data.get('title');
+//         }}
+//       />
+//       <Route
+//         path="/about"
+//         loader={aboutLoader}
+//         lazy={() => import('./route/About')}
+//       />
+//       <Route path="path-with-loader/:id?" element={<Payment />} loader={() => {
+//         return 'Lorem ipsum';
+//       }}/>
+//       <Route
+//         path="awaited"
+//         element={<AwaitedData />}
+//         loader={fakeLoader}
+//       />
+//     </Route>
+//   )
+// );
+//
+// const router3 = createBrowserRouter([
+//   {
+//     element: <MainLayout />,
+//     children: [
+//       {
+//         path: "/",
+//         element: <TasksView />,
+//         loader: tasksLoader,
+//       },{
+//         path: "create",
+//         element: <AddTaskView />,
+//         action: AddTaskAction,
+//       }, {
+//         path: ":id",
+//         element: <SingleTaskView />,
+//         loader: taskLoader,
+//         action: deleteTaskAction,
+//       }
+//       ],
+//   }
+//   ]);
 
-const router2 = createBrowserRouter(
-  createRoutesFromElements(
-    <Route element={<MainLayout />}>
-      <Route path="/" element={<Home />}/>
-      <Route
-        path="/create"
-        element={<Create />}
-        action={async ({request}) => {
-          const data = await request.formData();
-          console.log('Lorem ipsum', data.get('title'));
-          return data.get('title');
-        }}
-      />
-      <Route
-        path="/about"
-        loader={aboutLoader}
-        lazy={() => import('./route/About')}
-      />
-      <Route path="path-with-loader/:id?" element={<Payment />} loader={() => {
-        return 'Lorem ipsum';
-      }}/>
-      <Route
-        path="awaited"
-        element={<AwaitedData />}
-        loader={fakeLoader}
-      />
-    </Route>
-  )
-);
+const router = createRouter({
+  routeTree,
+});
 
-const router3 = createBrowserRouter([
-  {
-    element: <MainLayout />,
-    children: [
-      {
-        path: "/",
-        element: <TasksView />,
-        loader: tasksLoader,
-      },{
-        path: "create",
-        element: <AddTaskView />,
-        action: AddTaskAction,
-      }, {
-        path: ":id",
-        element: <SingleTaskView />,
-        loader: taskLoader,
-        action: deleteTaskAction,
-      }
-      ],
+declare module '@tanstack/react-router' {
+  interface Register {
+    router: typeof router,
   }
-  ]);
+}
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <RouterProvider router={router3} />
+    <RouterProvider router={router} />
+    {/*<RouterProvider router={router3} />*/}
     {/*<BrowserRouter>*/}
     {/*  <Routes>*/}
     {/*    <Route element={<MainLayout />}>*/}
