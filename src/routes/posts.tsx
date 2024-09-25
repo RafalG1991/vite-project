29dc52243e941fd8cd842acfbb9817e59490fe3d
Rafal <rgrajcar@gmail.com>
2024-09-25 20:56:36 +0200

    added Outlet to posts route

--- src/routes/posts.tsx
+++ src/routes/posts.tsx
@@ -1,11 +1,10 @@
-import { createFileRoute } from '@tanstack/react-router'
+import {createFileRoute, Outlet} from '@tanstack/react-router'
 
 const Posts = () => {
   return (
     <div>
       <h2>Posts:</h2>
-      <p>Post1</p>
-      <p>Post2</p>
+      <Outlet />
     </div>
   )
 }
