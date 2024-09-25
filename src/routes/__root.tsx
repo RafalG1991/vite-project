041b2a888cbdf7a58869940703946368d952cfd0
Rafal <rgrajcar@gmail.com>
2024-09-25 20:32:31 +0200

    added Link to Posts path

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,9 +1,16 @@
-import { Outlet, createRootRoute } from '@tanstack/react-router'
+import {Outlet, createRootRoute, Link} from '@tanstack/react-router'
 
 const RootComponent = () => {
   return (
     <div>
       <h1>Tanstack Router App</h1>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/posts">Posts</Link>
+          </li>
+        </ul>
+      </nav>
       <Outlet />
     </div>
   )
