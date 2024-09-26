470f83f61ed1371e28b4b6158136fe196b084a71
Rafal <rgrajcar@gmail.com>
2024-09-26 20:39:06 +0200

    added PostsNotFound as notFoundComponent

--- src/routes/_wrapper.posts.tsx
+++ src/routes/_wrapper.posts.tsx
@@ -9,6 +9,16 @@ const Posts = () => {
   )
 }
 
+const PostNotFound = () => {
+  return (
+    <div>
+      <h1>Post not found</h1>
+      <p>Post you are looking for is not here</p>
+    </div>
+  )
+}
+
 export const Route = createFileRoute('/_wrapper/posts')({
   component: Posts,
+  notFoundComponent: PostNotFound,
 })
