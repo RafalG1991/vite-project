258d51e941885a35df2829142e37cfae42b974aa
Rafal <rgrajcar@gmail.com>
2024-10-01 20:51:52 +0200

    added Pokemon type for returned data

--- src/routes/_wrapper/pokemon/-loaders/index.ts
+++ src/routes/_wrapper/pokemon/-loaders/index.ts
@@ -1,4 +1,6 @@
+import {Pokemon} from "../../../../types/pokemon.ts";
+
 export const getPokemon = async (pokemonName: string) => {
   const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
-  return response.json();
+  return response.json() as Promise<Pokemon>;
 }
