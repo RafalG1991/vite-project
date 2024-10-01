2a170e82f527fe30f7745c23f1930cb256a22884
Rafal <rgrajcar@gmail.com>
2024-10-01 20:52:09 +0200

    added SinglePokemon component to display loaded data

--- src/routes/_wrapper/pokemon/$pokemonName.tsx
+++ src/routes/_wrapper/pokemon/$pokemonName.tsx
@@ -1,7 +1,17 @@
 import { createFileRoute } from '@tanstack/react-router'
 import {getPokemon} from "./-loaders";
 
+const SinglePokemon = () => {
+  const {name, weight} = Route.useLoaderData();
+  return (
+    <div>
+      <p>Pokemon name: {name}</p>
+      <p>Pokemon weight: {weight}</p>
+    </div>
+  )
+}
+
 export const Route = createFileRoute('/_wrapper/pokemon/$pokemonName')({
   loader: ({params}) => getPokemon(params.pokemonName),
-  component: () => <div>Hello /_wrapper/pokemon/$pokemonName!</div>,
+  component: SinglePokemon,
 })
