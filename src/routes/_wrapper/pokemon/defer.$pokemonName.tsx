65a39ca54a291aa48a7a34219ddb0f46f0dad73b
Rafal <rgrajcar@gmail.com>
2024-10-05 01:49:57 +0200

    added Suspense and Await components to use deferred response data from loader

--- src/routes/_wrapper/pokemon/defer.$pokemonName.tsx
+++ src/routes/_wrapper/pokemon/defer.$pokemonName.tsx
@@ -1,18 +1,25 @@
-import { createFileRoute } from '@tanstack/react-router'
-import { getPokemon } from './-loaders'
+import { createFileRoute, Await } from '@tanstack/react-router'
+import {getDeferredPokemon} from './-loaders'
+import {Suspense} from "react";
 
 const SingleDeferredPokemon = () => {
-  const { name, weight } = Route.useLoaderData()
+  const { deferredResponse } = Route.useLoaderData()
 
   return (
-    <div>
-      <p>Pokemon name: {name}</p>
-      <p>Pokemon weight: {weight}</p>
-    </div>
+    <Suspense fallback="Loading data...">
+      <Await promise={deferredResponse}>
+        {({name, weight}) => (
+          <div>
+            <p>Pokemon name: {name}</p>
+            <p>Pokemon weight: {weight}</p>
+          </div>
+        )}
+      </Await>
+    </Suspense>
   )
 }
 
 export const Route = createFileRoute('/_wrapper/pokemon/defer/$pokemonName')({
-  loader: ({ params}) => getPokemon(params.pokemonName),
+  loader: ({ params}) => getDeferredPokemon(params.pokemonName),
   component: SingleDeferredPokemon,
 })
