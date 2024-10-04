d9d7852a915164738923ce469ca838ec7e74a517
Rafal <rgrajcar@gmail.com>
2024-10-05 01:37:10 +0200

    added new SingleDeferredPokemon component with loader

--- src/routes/_wrapper/pokemon/defer.$pokemonName.tsx
+++ src/routes/_wrapper/pokemon/defer.$pokemonName.tsx
@@ -1,5 +1,18 @@
 import { createFileRoute } from '@tanstack/react-router'
+import { getPokemon } from './-loaders'
+
+const SingleDeferredPokemon = () => {
+  const { name, weight } = Route.useLoaderData()
+
+  return (
+    <div>
+      <p>Pokemon name: {name}</p>
+      <p>Pokemon weight: {weight}</p>
+    </div>
+  )
+}
 
 export const Route = createFileRoute('/_wrapper/pokemon/defer/$pokemonName')({
-  component: () => <div>Hello /_wrapper/pokemon/defer/$pokemonName!</div>,
+  loader: ({ params}) => getPokemon(params.pokemonName),
+  component: SingleDeferredPokemon,
 })
