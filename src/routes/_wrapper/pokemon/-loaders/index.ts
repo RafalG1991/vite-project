87d5d75aa5cd3e3e6d4e38a8634385a53aea68c2
Rafal <rgrajcar@gmail.com>
2024-10-05 01:45:47 +0200

    added getDeferredPokemon function to return response from apiCall as object with defer method

--- src/routes/_wrapper/pokemon/-loaders/index.ts
+++ src/routes/_wrapper/pokemon/-loaders/index.ts
@@ -1,6 +1,15 @@
 import {Pokemon} from "../../../../types/pokemon.ts";
+import {apiCall} from "../../../../utils/apiCall.ts";
+import {defer} from "@tanstack/react-router";
 
 export const getPokemon = async (pokemonName: string) => {
   const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
   return response.json() as Promise<Pokemon>;
+}
+
+export const getDeferredPokemon = async (pokemonName: string) => {
+  const responsePromise = apiCall<Pokemon>(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
+  return {
+    deferredResponse: defer(responsePromise),
+  }
 }
