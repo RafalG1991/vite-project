ecccac1d9a3f5c5729b12c75e14142bc8d14d54e
Rafal <rgrajcar@gmail.com>
2024-10-07 19:36:56 +0200

    added size property function to fetch records per page

--- src/routes/_wrapper/people/-loaders/index.ts
+++ src/routes/_wrapper/people/-loaders/index.ts
@@ -1,6 +1,6 @@
 import {PaginatedPeople} from "../../../../types/people.ts";
 
-export const peopleLoader = async (page: number) => {
-  const response = await fetch(`http://localhost:3000/people?_page=${page}`);
+export const peopleLoader = async (page: number, size: number) => {
+  const response = await fetch(`http://localhost:3000/people?_page=${page}&_per_page=${size}`);
   return response.json() as Promise<PaginatedPeople>;
 }
