0ae634e596bfcb55e1b6a12c2cbd6617b041f1c5
Rafal <rgrajcar@gmail.com>
2024-10-08 22:05:44 +0200

    added IP component to display ip address from fetch api

--- src/routes/ip.tsx
+++ src/routes/ip.tsx
@@ -1,6 +1,12 @@
 import { createFileRoute } from '@tanstack/react-router'
 
 const IP = () => {
+  const { ip } = Route.useLoaderData();
+
+  return <div>
+    <h1>Your IP:</h1>
+    <h2>{ip}</h2>
+  </div>
 
 }
 
