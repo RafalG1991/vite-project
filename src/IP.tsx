939f1f58d6e901c5ff919b3dfbb3376a44d560bf
Rafal <rgrajcar@gmail.com>
2024-10-09 20:26:55 +0200

    added hook useRouteContext to read the context value

--- src/IP.tsx
+++ src/IP.tsx
@@ -3,12 +3,14 @@ import {getRouteApi} from "@tanstack/react-router";
 const IpRoute = getRouteApi('/ip');
 
 export function IP () {
-  const { ip } = IpRoute.useLoaderData()
+  const { ip } = IpRoute.useLoaderData();
+  const { value } = IpRoute.useRouteContext();
 
   return (
     <div>
       <h1>Your IP:</h1>
       <h2>{ip}</h2>
+      <h3>Context value: {value}</h3>
     </div>
   )
 }
