7d22c58aaa9ab476e184af17a2c214b20e9a170c
Rafal <rgrajcar@gmail.com>
2024-10-20 19:38:43 +0200

    added SingleUser array as a returned type from function getUsers and filled some dummy users data to be returned from the hook's function

--- src/hooks/useUsers.ts
+++ src/hooks/useUsers.ts
@@ -1,5 +1,12 @@
+import {SingleUser} from "../types/user.ts";
+
 export const useUsers = () => {
-  const getUsers = () => [];
+  const getUsers = (): SingleUser[] => [
+    {id: 1, name: 'Alex', lastName: 'John', age: 15},
+    {id: 2, name: 'John', lastName: 'Smith', age: 25},
+    {id: 3, name: 'Maria', lastName: 'Carry', age: 40},
+    {id: 4, name: 'Anna', lastName: 'Newton', age: 33},
+  ];
 
   return {
     getUsers,
