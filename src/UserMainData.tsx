3ba57d9947c3cf8eddda4483f0df549871ee25b9
Rafal <rgrajcar@gmail.com>
2024-10-20 21:46:06 +0200

    added UserMainData component structure to display user's first and last name in different components

--- src/UserMainData.tsx
+++ src/UserMainData.tsx
@@ -1,3 +1,14 @@
-export const UserMainData = () => {
+import {SingleUser} from "./types/user.ts";
 
+export type UserMainDataProps = {
+  user: SingleUser;
+}
+
+export const UserMainData = ({ user }: UserMainDataProps) => {
+  const {name, lastName} = user;
+
+  return <div>
+    <UserName name={name}/>
+    <UserLastName lastName={lastName}/>
+  </div>
 }
