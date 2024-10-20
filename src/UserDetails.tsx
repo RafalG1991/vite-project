5bf5bd9906e9c15afff639d9669ddf8de3fb8b6e
Rafal <rgrajcar@gmail.com>
2024-10-20 21:45:34 +0200

    added UserDetails component structure to display info about single user

--- src/UserDetails.tsx
+++ src/UserDetails.tsx
@@ -1,3 +1,13 @@
-export const UserDetails = () => {
+import {SingleUser} from "./types/user.ts";
 
+export type UserDetailsProps = {
+  user: SingleUser;
+}
+
+export const UserDetails = ({ user }: UserDetailsProps) => {
+  const {name, lastName, age} = user;
+
+  return <div>
+    <small>{name} {lastName} is {age} years old.</small>
+  </div>
 }
