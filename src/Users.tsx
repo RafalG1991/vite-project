e64ddd6a94be2b36b70a88a32dd3fa5b5a50927c
Rafal <rgrajcar@gmail.com>
2024-10-20 21:23:50 +0200

    added UsersList component as a return of Users component with users props

--- src/Users.tsx
+++ src/Users.tsx
@@ -1,7 +1,10 @@
 import {useUsers} from "./hooks/useUsers.ts";
+import {UsersList} from "./UsersList.tsx";
 
 export const Users = () => {
   const {getUsers} = useUsers();
 
   const users = getUsers();
+
+  return <UsersList users={users} />
 }
