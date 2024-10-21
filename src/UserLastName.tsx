2987b603a1b1ae2383f3d6a0941e9f7cb991f1cc
Rafal <rgrajcar@gmail.com>
2024-10-21 21:26:52 +0200

    added useUserContext custom hook to get first and last name

--- src/UserLastName.tsx
+++ src/UserLastName.tsx
@@ -1,3 +1,11 @@
+import {useUserContext} from "./context/useUserContext.tsx";
+
 export const UserLastName = () => {
+  const {user} = useUserContext();
+
+  if(!user) return null;
+
+  const {lastName} = user;
+
   return <p><strong>Last name:</strong> {lastName}</p>
 }
--- src/UserName.tsx
+++ src/UserName.tsx
@@ -1,3 +1,11 @@
+import {useUserContext} from "./context/useUserContext.tsx";
+
 export const UserName = () => {
+  const {user} = useUserContext();
+
+  if(!user) return null;
+
+  const {name} = user;
+
   return <p><strong>First name:</strong> {name}</p>
 }
