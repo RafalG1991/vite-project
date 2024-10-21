9379037bd2b11128a8b60464f108ace242176298
Rafal <rgrajcar@gmail.com>
2024-10-21 21:21:38 +0200

    added useContext hook to get data from provider

--- src/UserDetails.tsx
+++ src/UserDetails.tsx
@@ -1,4 +1,11 @@
+import {useContext} from "react";
+import {UserContext} from "./context/UserContext.tsx";
+
 export const UserDetails = () => {
+  const {user} = useContext(UserContext);
+
+  if(!user) return null;
+
   const {name, lastName, age} = user;
 
   return <div>
