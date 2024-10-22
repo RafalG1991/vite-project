188f0f1f67029afedad7ec19af392d0b4a3557fe
Rafal <rgrajcar@gmail.com>
2024-10-22 21:47:55 +0200

    added store provider for Users component

--- src/Users.tsx
+++ src/Users.tsx
@@ -1,10 +1,14 @@
 import {useUsers} from "./hooks/useUsers.ts";
 import {UsersList} from "./UsersList.tsx";
+import {Provider} from "react-redux";
+import {store} from "./store";
 
 export const Users = () => {
   const {getUsers} = useUsers();
 
   const users = getUsers();
 
-  return <UsersList users={users} />
+  return <Provider store={store}>
+    <UsersList users={users} />
+  </Provider>
 }
