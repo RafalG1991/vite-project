14358fd5b1bbda7d2e9feb265d2901e9b16daf9b
Rafal <rgrajcar@gmail.com>
2024-10-22 21:54:47 +0200

    added case when product doesn't exist

--- src/Product.tsx
+++ src/Product.tsx
@@ -4,6 +4,8 @@ import {RootState} from "./store";
 export const Product = () => {
   const {name, price} = useSelector((state: RootState) => state.product);
 
+  if(!name) return <h1>Product doesn't exist!</h1>
+
   return <div>
     <h1>{name}</h1>
     <h2>{price}</h2>
