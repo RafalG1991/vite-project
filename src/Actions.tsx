ed86670c775c473d1389e4a29fb73cc3ca6d4948
Rafal <rgrajcar@gmail.com>
2024-10-23 22:00:10 +0200

    added addNewProduct function with new button to add new product and dispatch addProduct reducer

--- src/Actions.tsx
+++ src/Actions.tsx
@@ -1,5 +1,6 @@
 import {useDispatch} from "react-redux";
 import {setName, setPrice} from "./slices/productSlice.ts";
+import {addProduct} from "./slices/productsSlice.ts";
 
 export const Actions = () => {
   const dispatch = useDispatch();
@@ -12,8 +13,13 @@ export const Actions = () => {
     dispatch(setPrice(123));
   }
 
+  const addNewProduct = () => {
+    dispatch(addProduct('Product #777'));
+  }
+
   return <div>
     <button onClick={setProductName}>Set product name</button>
     <button onClick={setProductPrice}>Set product price</button>
+    <button onClick={addNewProduct}>Add new product</button>
   </div>
 }
