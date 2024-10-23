ae49e52d48e3c0d204c979dae4fbb15c34f29a31
Rafal <rgrajcar@gmail.com>
2024-10-23 21:49:55 +0200

    added productsReducer to store, cleaned up the component from commented structure

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,22 +1,12 @@
 import {configureStore} from "@reduxjs/toolkit";
 import {productReducer} from "../slices/productSlice.ts";
+import {productsReducer} from "../slices/productsSlice.ts";
 
 export const store = configureStore({
   reducer: {
     product: productReducer,
+    products: productsReducer,
   }
 });
 
 export type RootState = ReturnType<typeof store.getState>;
-
-
-// {
-//   product: {
-//     price: number,
-//     name: string,
-//   },
-//   products: {
-//     list: string[],
-//     count: number,
-//   },
-// }
