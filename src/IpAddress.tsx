b76716a14ad5fb817e36bab9a92ea2898f0ba211
Rafal <rgrajcar@gmail.com>
2024-10-24 22:42:25 +0200

    added return for loading, error and successful fetch resolve

--- src/IpAddress.tsx
+++ src/IpAddress.tsx
@@ -4,4 +4,13 @@ import {RootState} from "./store";
 export const IpAddress = () => {
   const {value, error, loading} = useSelector((state: RootState) => state.ip);
 
+  if(loading) return <h1>Loading ip address...</h1>
+  if(error) return <h1>{error}</h1>
+
+  return (
+    <div>
+      <h1>Your IP address:</h1>
+      <h2>{value}</h2>
+    </div>
+  )
 }
