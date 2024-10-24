387f305dc88c9d8159e1ffdbbdce14a4bcdfbcd4
Rafal <rgrajcar@gmail.com>
2024-10-24 22:36:22 +0200

    added ipReducer to store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,12 +1,14 @@
 import {configureStore} from "@reduxjs/toolkit";
 import {productReducer} from "../slices/productSlice.ts";
 import {productsReducer} from "../slices/productsSlice.ts";
+import {ipReducer} from "../slices/ipSlice.ts";
 
 export const store = configureStore({
   devTools: false,
   reducer: {
     product: productReducer,
     products: productsReducer,
+    ip: ipReducer,
   }
 });
 
