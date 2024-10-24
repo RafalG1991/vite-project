1c7dfc69a54e65854d6b14c5f94b4fe461bd9ee9
Rafal <rgrajcar@gmail.com>
2024-10-24 22:50:46 +0200

    added AppDispatch type useDispatch and added getIpAddress to dispatch in function

--- src/Actions.tsx
+++ src/Actions.tsx
@@ -1,9 +1,11 @@
 import {useDispatch} from "react-redux";
 import {setName, setPrice} from "./slices/productSlice.ts";
 import {addProduct} from "./slices/productsSlice.ts";
+import {AppDispatch} from "./store";
+import {getIpAddress} from "./slices/ipSlice.ts";
 
 export const Actions = () => {
-  const dispatch = useDispatch();
+  const dispatch = useDispatch<AppDispatch>();
 
   const setProductName = () => {
     dispatch(setName('Product #1234'));
@@ -18,7 +20,7 @@ export const Actions = () => {
   }
 
   const loadIpAddress = () => {
-
+    dispatch(getIpAddress());
   }
 
   return <div>
