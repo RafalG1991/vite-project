c03f6e4b37f0610124f8428cb5cd23f1f222b5a5
Rafal <rgrajcar@gmail.com>
2024-10-25 22:30:24 +0200

    added IpAddress type

--- src/slices/ipRtkSlice.ts
+++ src/slices/ipRtkSlice.ts
@@ -1,10 +1,14 @@
 import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query";
 
+type IpAddress = {
+  ip: string;
+}
+
 export const ipRtkSlice = createApi({
   reducerPath: 'ipApi',
   baseQuery: fetchBaseQuery({baseUrl: 'https://api.ipify.org'}),
   endpoints: (builder) => ({
-    getIpAddress: builder.query({
+    getIpAddress: builder.query<IpAddress, void>({
       query: () => '?format=json',
     })
   }),
