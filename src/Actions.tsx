36e79f046e5828dbd0f76f239f9975c8671248cc
Rafal <rgrajcar@gmail.com>
2024-10-27 21:39:18 +0100

    added two buttons to refetch data and added first method to use refetch method from useGetIpAddressQuery hook

--- src/Actions.tsx
+++ src/Actions.tsx
@@ -2,9 +2,12 @@ import {setName, setPrice} from "./slices/productSlice.ts";
 import {addProduct} from "./slices/productsSlice.ts";
 import {getIpAddress} from "./slices/ipSlice.ts";
 import {useAppDispatch} from "./hooks/useAppDispatch.ts";
+import {useGetIpAddressQuery} from "./slices/ipRtkSlice.ts";
 
 export const Actions = () => {
   const dispatch = useAppDispatch();
+
+  const { refetch } = useGetIpAddressQuery();
 
   const setProductName = () => {
     dispatch(setName('Product #1234'));
@@ -22,10 +25,20 @@ export const Actions = () => {
     dispatch(getIpAddress());
   }
 
+  const handleFirstRefetch = () => {
+    refetch();
+  }
+
+  const handleSecondRefetch = () => {
+
+  }
+
   return <div>
     <button onClick={setProductName}>Set product name</button>
     <button onClick={setProductPrice}>Set product price</button>
     <button onClick={addNewProduct}>Add new product</button>
     <button onClick={loadIpAddress}>Get IP address now!</button>
+    <button onClick={handleFirstRefetch}>Refetch #1</button>
+    <button onClick={handleSecondRefetch}>Refetch #2</button>
   </div>
 }
