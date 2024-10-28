66dc4692c99050469be0b2c4e6699534eff67f65
Rafal <rgrajcar@gmail.com>
2024-10-28 21:15:54 +0100

    added delete button and handleDelete method using useDeleteTodoMutation

--- src/SingleTodo.tsx
+++ src/SingleTodo.tsx
@@ -1,11 +1,21 @@
 import {Todo} from "./types/todo.ts";
+import {useDeleteTodoMutation} from "./slices/todosApiSlice.ts";
 
 export type SingleTodoProps = {
   todo: Todo;
 }
 
 export const SingleTodo = ({todo}: SingleTodoProps) => {
+  const [deleteTodo] = useDeleteTodoMutation();
+
+  const handleDelete = () => {
+    deleteTodo(todo.id);
+  }
+
   return (
-    <li>{todo.title}</li>
+    <li>
+      <strong>{todo.title}</strong>
+      <button onClick={handleDelete}>Delete</button>
+    </li>
   )
 }
