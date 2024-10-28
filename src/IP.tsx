6e9cfcb4843396ece6efa3f3e59fb90b3e3f8fda
Rafal <rgrajcar@gmail.com>
2024-10-28 20:39:13 +0100

    added TodoList component to IP component inside Provider

--- src/IP.tsx
+++ src/IP.tsx
@@ -1,4 +1,7 @@
 import {getRouteApi} from "@tanstack/react-router";
+import {Provider} from "react-redux";
+import {todosStore} from "./store/todosStore.ts";
+import {TodoList} from "./TodoList.tsx";
 
 const IpRoute = getRouteApi('/ip');
 
@@ -7,10 +10,15 @@ export function IP () {
   const { value } = IpRoute.useRouteContext();
 
   return (
-    <div>
-      <h1>Your IP:</h1>
-      <h2>{ip}</h2>
-      <h3>Context value: {value}</h3>
-    </div>
+    <>
+      <Provider store={todosStore}>
+        <TodoList />
+      </Provider>
+      <div>
+        <h1>Your IP:</h1>
+        <h2>{ip}</h2>
+        <h3>Context value: {value}</h3>
+      </div>
+    </>
   )
 }
