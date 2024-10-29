361aa2ea856a26e7458c36aec9992879e0060216
Rafal <rgrajcar@gmail.com>
2024-10-29 20:59:49 +0100

    added ip property as string type

--- src/store/useFooStore.ts
+++ src/store/useFooStore.ts
@@ -3,10 +3,12 @@ import {create} from "zustand";
 type FooStore = {
   value: number;
   setValue: (newValue: number) => void;
+  ip: string,
 }
 
 export const useFooStore = create<FooStore>(set => ({
   value: 0,
+  ip: '',
   setValue: (newValue: number) => {
     set({
       value: newValue,
