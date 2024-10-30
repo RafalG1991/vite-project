575c2f5b7b1f29e1c9d20b49f7cba25f1c99acfd
Rafal <rgrajcar@gmail.com>
2024-10-30 19:37:08 +0100

    added Container component to __root and moved all files to Container directory

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,12 +1,13 @@
 import {createRootRouteWithContext, Outlet} from '@tanstack/react-router'
+import {Container} from "../components/Container";
 
 const RootComponent = () => {
 
   return (
-    <div>
+    <Container>
       <h1>Tanstack Router App</h1>
       <Outlet />
-    </div>
+    </Container>
   )
 }
 
