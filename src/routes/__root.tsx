b11146bdea85ccd2d6549524ad8f1448731af4e2
Rafal <rgrajcar@gmail.com>
2024-10-30 19:19:01 +0100

    cleaned up __root file

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,78 +1,10 @@
-import {Outlet, Link, createRootRouteWithContext} from '@tanstack/react-router'
-// import { TanStackRouterDevtools } from '@tanstack/router-devtools'
-import { Route as postsRoute} from './_wrapper/posts.tsx'
-import React, {Suspense} from "react";
-// import {useEffect} from "react";
-
-const TanStackRouterDevtools = import.meta.env.DEV ?
-  React.lazy(() => import('@tanstack/router-devtools').then((res) => ({
-    default: res.TanStackRouterDevtools
-  }))) : () => null;
+import { createRootRouteWithContext} from '@tanstack/react-router'
 
 const RootComponent = () => {
-  // const navigate = useNavigate();
-
-  // useEffect(() => {
-  //   navigate({ to: '/posts'});
-  // }, []);
 
   return (
     <div>
       <h1>Tanstack Router App</h1>
-      <nav>
-        <ul>
-          <li>
-            <Link to="..">Home</Link>
-          </li>
-          <li>
-            <Link to="/posts">Posts</Link>
-          </li>
-          <li>
-            <Link to={postsRoute.fullPath}>Posts</Link>
-          </li>
-          <li>
-            <Link to={"/ip"}>IP</Link>
-          </li>
-          <li>
-            <Link
-              to="/pokemon/$pokemonName"
-              params={{pokemonName: 'pikachu'}}
-              search={{
-                page: 1,
-                pageSize: 10,
-                filters: ['oldest', 'desc'],
-              }}
-            >Pikachu</Link>
-          </li>
-          <li>
-            <Link
-              to="/pokemon/$pokemonName"
-              params={{pokemonName: 'charizard'}}
-              search={{
-                page: 1,
-                pageSize: 10,
-                filters: ['new', 'asc'],
-              }}
-            >Charizard</Link>
-          </li>
-          <li>
-            <Link
-              to="/pokemon/defer/$pokemonName"
-              params={{pokemonName: 'charizard'}}
-            >Deferred Charizard</Link>
-          </li>
-          <li>
-            <Link
-              to="/people"
-              search={{page: 1, size: 10}}
-            >People</Link>
-          </li>
-        </ul>
-      </nav>
-      <Outlet/>
-      <Suspense>
-        <TanStackRouterDevtools/>
-      </Suspense>
     </div>
   )
 }
