53ddd89e3b626e23dc113560c1f775fd4ed2d907
Rafal <rgrajcar@gmail.com>
2024-10-30 20:35:09 +0100

    added handleSubmit method to use setOrderData to save the title and config from form

--- src/Pages/Order.tsx
+++ src/Pages/Order.tsx
@@ -1,21 +1,34 @@
 import {PageHeader} from "../components/PageHeader";
 import {useInput} from "../hooks/useInput.ts";
+import {FormEvent} from "react";
+import {useOrderStore} from "../store/useOrderStore.ts";
+import {useShallow} from "zustand/react/shallow";
 
 export const Order = () => {
-  const titleInput = useInput('');
-  const descriptionInput = useInput('');
+  const { order, setOrderData } = useOrderStore(useShallow(state => ({ order: state.order, setOrderData: state.setOrderData})));
+  const titleInput = useInput(order.title);
+  const descriptionInput = useInput(order.configuration);
+
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    setOrderData({
+      title: titleInput.value,
+      configuration: descriptionInput.value,
+    });
+  }
 
   return (
     <>
       <PageHeader>Order</PageHeader>
       <p>Please fill your order configuration</p>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div>
           <input type="text" name="title" placeholder="Title" {...titleInput} />
         </div>
         <div>
           <textarea placeholder="Configuration" {...descriptionInput} />
         </div>
+        <button type="submit" disabled={!titleInput.value || !descriptionInput.value}>Proceed to next step...</button>
       </form>
     </>
   )
