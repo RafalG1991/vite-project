575e3f7c2101decef61388ae631af1fee873a267
Rafal <rgrajcar@gmail.com>
2024-10-30 20:15:39 +0100

    added useInput custom hook Order component to handle changes in title input and description textarea

--- src/Pages/Order.tsx
+++ src/Pages/Order.tsx
@@ -1,16 +1,20 @@
 import {PageHeader} from "../components/PageHeader";
+import {useInput} from "../hooks/useInput.ts";
 
 export const Order = () => {
+  const titleInput = useInput('');
+  const descriptionInput = useInput('');
+
   return (
     <>
       <PageHeader>Order</PageHeader>
       <p>Please fill your order configuration</p>
       <form>
         <div>
-          <input type="text" name="title" placeholder="Title"/>
+          <input type="text" name="title" placeholder="Title" {...titleInput} />
         </div>
         <div>
-          <textarea placeholder="Configuration"/>
+          <textarea placeholder="Configuration" {...descriptionInput} />
         </div>
       </form>
     </>
