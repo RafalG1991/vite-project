d768c849ab0beef150bf3c12d6901c4c7c46dc4a
Rafal <rgrajcar@gmail.com>
2024-10-30 20:27:53 +0100

    added userOrder store with zustand

---
+++ src/store/useOrderStore.ts
@@ -0,0 +1,39 @@
+import {create} from "zustand";
+
+type OrderState = {
+  order: {
+    title: string;
+    configuration: string;
+  },
+  shipping: {
+    city: string;
+    street: string;
+    postalCode: string;
+  },
+  summary: {
+    comment: string;
+  }
+}
+
+type SetOrderDataAction = {
+  title: string;
+  configuration: string;
+}
+
+export const useOrderStore = create<OrderState>()(set => ({
+  order: {
+    title: '',
+    configuration: '',
+  },
+  shipping: {
+    city: '',
+    street: '',
+    postalCode: '',
+  },
+  summary: {
+    comment: '',
+  },
+  setOrderData: (payload: SetOrderDataAction) => set({
+    order: payload,
+  }),
+}))
