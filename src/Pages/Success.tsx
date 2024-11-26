56ef43864f055de36dbda83ddda46d8c2d111629
Rafal <rgrajcar@gmail.com>
2024-11-26 19:44:58 +0100

    added placeOrder to Success component to reset the order after mounting the component

--- src/Pages/Success.tsx
+++ src/Pages/Success.tsx
@@ -1,7 +1,14 @@
 import {PageHeader} from "../components/PageHeader";
 import {Link} from "@tanstack/react-router";
+import {useEffect} from "react";
+import {useOrderStore} from "../store/useOrderStore.ts";
 
 export const Success = () => {
+  const placeOrder = useOrderStore(state => state.placeOrder);
+
+  useEffect(() => {
+    placeOrder();
+  }, []);
   return (
     <>
       <PageHeader>Transaction successful!</PageHeader>
