4c848ad1387f0e878e7211704b30105fdb89d7d6
Rafal <rgrajcar@gmail.com>
2024-12-01 20:31:50 +0100

    added loginSchema for login and password validation

--- src/Pages/YupForm.tsx
+++ src/Pages/YupForm.tsx
@@ -1,3 +1,10 @@
+import {object, string} from "yup";
+
+const loginSchema = object({
+  login: string().required().min(5),
+  password: string().required().min(5),
+})
+
 export const YupForm = () => {
   return null;
 }
