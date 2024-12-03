d8b3b5f8cfb3a9cd934209b66938d9cce133458b
Rafal <rgrajcar@gmail.com>
2024-12-03 20:26:49 +0100

    added new InfoCard component with props type

---
+++ src/components/InfoCard/InfoCard.tsx
@@ -0,0 +1,13 @@
+export type InfoCardProps = {
+  title: string;
+  description: string;
+  label: string;
+}
+
+export const InfoCard = ({title, description, label}: InfoCardProps) => {
+  return (
+    <div>
+
+    </div>
+  )
+}
