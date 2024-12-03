a1e8bac4150b4ebae998f1109cacb424a585ed68
Rafal <rgrajcar@gmail.com>
2024-12-03 21:17:02 +0100

    cleaned up InfoCard component from unused props

--- src/components/InfoCard/InfoCard.tsx
+++ src/components/InfoCard/InfoCard.tsx
@@ -1,22 +1,11 @@
 import styles from './styles.module.scss';
-import {Title} from "./components/Title/Title.tsx";
 import {ReactNode} from "react";
 
 export type InfoCardProps = {
   children: ReactNode;
-  item: {
-    title?: string;
-    description?: string;
-    label?: string;
-  }
 }
 
-export const InfoCard = ({
-  children,
-  item: {
-    title, description, label
-  }
-}: InfoCardProps) => {
+export const InfoCard = ({children}: InfoCardProps) => {
   return (
     <div className={styles.infoBox}>
       {children}
