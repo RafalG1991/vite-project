a57d0f5ca2e1a0d27329c46d26c95062f0b03a43
Rafal <rgrajcar@gmail.com>
2024-12-05 21:20:48 +0100

    added active property to config object state

--- src/Foo.tsx
+++ src/Foo.tsx
@@ -2,18 +2,19 @@ import {useEffect, useLayoutEffect, useRef, useState} from "react";
 
 export const Foo = () => {
   const ref = useRef<HTMLHeadingElement>(null);
-  const [config, setConfig] = useState<{width: number, height: number, x: number, y: number}>({
+  const [config, setConfig] = useState<{width: number, height: number, x: number, y: number, active: boolean}>({
     x: 0,
     y: 0,
     width: 200,
     height: 100,
+    active: false,
   });
 
   useEffect(() => {
     console.log("USE EFFECT", ref.current);
     if(!ref.current) return;
     const { width, height, x, y} = ref.current?.getBoundingClientRect();
-    setConfig({width, height, x, y});
+    setConfig({width, height, x, y, active: true});
   }, []);
 
   useLayoutEffect(() => {
@@ -30,7 +31,7 @@ export const Foo = () => {
         height: config.height,
         width: config.width,
         zIndex: -1,
-        background: 'blue'
+        background: config.active ? 'red' : 'blue'
       }}/>
     </div>
 
