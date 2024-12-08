08750895c5b2dcb5c665c146ae9eebeb4d334de2
Rafal <rgrajcar@gmail.com>
2024-12-08 21:23:29 +0100

    added elements type as an array of objects and iterated through the array to display li elements

--- src/Slow.tsx
+++ src/Slow.tsx
@@ -1,11 +1,11 @@
 import {useState} from "react";
 
 export const Slow = () => {
-  const [elements, setElements] = useState<string[]>([]);
+  const [elements, setElements] = useState<{label: string, id: number}[]>([]);
 
   return (
     <ul>
-
+      {elements.map(el => <li key={el.id}>{el.label}</li>)}
     </ul>
   )
 
