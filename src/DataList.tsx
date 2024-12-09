85b6ce41b85b8915bc42a907ebeddbec1af6cf2b
Rafal <rgrajcar@gmail.com>
2024-12-09 22:02:50 +0100

    added RowComponent to DataList component

--- src/DataList.tsx
+++ src/DataList.tsx
@@ -1,17 +1,22 @@
-import {ReactNode} from "react";
+import {FunctionComponent, ReactNode} from "react";
 
 export type DataListProps<T> = {
   items: T[];
   callback: (item: T) => void;
+  RowComponent?: FunctionComponent;
 }
 
-export const DataList = <T extends ReactNode>({ items, callback }: DataListProps<T>) => {
+export const DataList = <T extends ReactNode>({ items, callback, RowComponent }: DataListProps<T>) => {
   return (
     <ul>
-      {items.map((item, index) => <li key={index}>
-        {item}
-        <button onClick={() => callback(item)}>Click</button>
-      </li>)}
+      {items.map((item, index) => (
+          !!RowComponent ?
+            <RowComponent />
+            : <li key={index}>
+            {item}
+            <button onClick={() => callback(item)}>Click</button>
+          </li>
+      ))}
     </ul>
   )
 }
