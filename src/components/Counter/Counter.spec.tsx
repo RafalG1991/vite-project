cf2ab54f6cf2efd83d3c8945c55bf406e0934a2a
Rafal <rgrajcar@gmail.com>
2024-12-24 10:40:02 +0100

    added second test for Counter component with clicking the button to increment the counter value

--- src/components/Counter/Counter.spec.tsx
+++ src/components/Counter/Counter.spec.tsx
@@ -1,6 +1,9 @@
 import {render, screen} from '@testing-library/react';
 import '@testing-library/jest-dom';
+import userEvent from '@testing-library/user-event';
 import {Counter} from "./Counter.tsx";
+
+const fireEvent = userEvent.setup();
 
 describe('Counter', () => {
   it('initial value should be rendered', () => {
@@ -8,5 +11,14 @@ describe('Counter', () => {
     const counterElement = screen.getAllByTestId('counter-value');
 
     expect(counterElement).toBeInTheDocument();
+  })
+  it('should increment the counter after clicking the button', async () => {
+    render(<Counter />);
+    const counterElement = screen.getByTestId('counter-value');
+    const buttonElement = screen.getByTestId('counter-button');
+
+    await fireEvent.click(buttonElement);
+
+    expect(counterElement).toHaveTextContent('1');
   })
 })
